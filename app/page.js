f05110492dd8cf0f0faba8370119c6d995bcef65
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-07-31 23:26:40 +0600

    added next-auth for authentication

--- app/page.js
+++ app/page.js
@@ -2,6 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <div className='bg-red-500'> test</div>
+    <div className='bg-blue-900 w-screen h-screen flex items-center'>
+      <div className='text-center w-full'>
+        <button className='bg-white p-2 px-4 rounded-lg'>Login with google</button>
+      </div>
+    </div>
   )
 }
