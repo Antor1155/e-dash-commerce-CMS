75ec7e5b5bd67787df2399865db60a99cc4999b7
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-11 01:35:16 +0600

    added all catagory to select as parent catagory when adding new catagory, and all catagories and shown below in table

--- app/api/catagories/route.js
+++ app/api/catagories/route.js
@@ -3,14 +3,29 @@ import { mongooseConnect } from "../../../database/mongoose"
 
 export const POST = async (req, res) =>{
     const {name} = await req.json()
-    
+
     try{
         await mongooseConnect()
         const CatagoryDoc = await Catagory.create({name})
 
-        return new Response(JSON.stringify(CatagoryDoc, {status: 200}))
+        return new Response(JSON.stringify(CatagoryDoc), {status: 200})
     }catch(error){
-        console.log("error in api/catagory ***, ")
+        console.log("error in api/catagory/post ***, ")
+    }
+
+}
+
+export const GET = async (req, res) =>{
+    // const params = new URL(req.url).searchParams
+    // const id = params.get("id")
+    
+    try{
+        await mongooseConnect()
+        const result = await Catagory.find()
+
+        return new Response(JSON.stringify(result), {status: 200})
+    }catch(error){
+        console.log("error in api/catagory/get ***, ")
     }
 
 }
--- app/catagories/page.js
+++ app/catagories/page.js
@@ -1,26 +1,62 @@
 "use client"
 import axios from "axios"
-import {useState} from "react"
+import { useState, useEffect } from "react"
 
 const Catagories = () => {
     const [name, setName] = useState("")
+    const [allCatagories, setAllCatagories] = useState([])
 
-    async function handleSubmit (e){
+    async function handleSubmit(e) {
         e.preventDefault()
-        const res = await axios.post("api/catagories", {name})
+        const res = await axios.post("/api/catagories", { name })
         setName("")
 
-        console.log(res.data)
+        fetchCatagories()
     }
+
+    useEffect(() => {
+        fetchCatagories()
+    }, [])
+
+    function fetchCatagories (){
+        axios.get("/api/catagories").then(result => setAllCatagories(result.data)).catch(error => console.log(error))
+    }
+
     return (
         <>
             <h1>Catagories</h1>
             <label>New Catagory name</label>
             <form className='flex gap-1' onSubmit={handleSubmit}>
-                <input type='text' placeholder="catagory name "  className='mb-0' value={name} onChange={(e)=>setName(e.target.value)}/>
+                <input type='text' placeholder="catagory name " className='mb-0' value={name} onChange={(e) => setName(e.target.value)} />
+
+                <select className="mb-0">
+                    <option value="">
+                        No parent
+                    </option>
+                    {allCatagories.length && allCatagories.map(catagory => (
+                        <option key={catagory._id} value={catagory._id}>
+                            {catagory.name}
+                        </option>
+                    ))}
+                </select>
 
                 <button type='submit' className='btn-primary'> Save</button>
             </form>
+
+            <table className="basic mt-2">
+                <thead>
+                    <tr>
+                        <td> Catagory name</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allCatagories.length && allCatagories.map(catagory => (
+                        <tr key={catagory._id}>
+                            <td> {catagory.name} </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
         </>
     )
