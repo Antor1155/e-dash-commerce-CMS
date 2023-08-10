d3f65224448724109bd31ee8fadb9219f6871de7
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-11 00:41:23 +0600

    added catagory secton to the nav , created page for catagory route, updated api/ catagory/route.js to handle post request

---
+++ app/catagories/page.js
@@ -0,0 +1,26 @@
+"use client"
+import axios from "axios"
+import {useState} from "react"
+
+const Catagories = () => {
+    const [name, setName] = useState("")
+
+    async function handleSubmit (){
+        await axios.post("api/catagories", {name})
+        setName("")
+    }
+    return (
+        <>
+            <h1>Catagories</h1>
+            <label>New Catagory name</label>
+            <form className='flex gap-1' onSubmit={handleSubmit}>
+                <input type='text' placeholder="catagory name "  className='mb-0' value={name} onChange={(e)=>setName(e.target.value)}/>
+
+                <button type='submit' className='btn-primary'> Save</button>
+            </form>
+
+        </>
+    )
+}
+
+export default Catagories
