8a9e82cdfac3a83c839655f1bec88ef15aec483e
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-13 00:41:29 +0600

    delete page work properly while deleting images from storageCloud and database informations

--- app/products/delete/[id]/page.js
+++ app/products/delete/[id]/page.js
@@ -3,11 +3,12 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
+import Image from "next/image"
 
 const DeleteProduct = ({ params }) => {
     const router = useRouter()
 
-    const [product, setProduct] = useState()
+    const [product, setProduct] = useState({})
 
     useEffect(() => {
         if (params.id) {
@@ -17,30 +18,46 @@ const DeleteProduct = ({ params }) => {
         }
     }, [params.id])
 
-    const handleDelete = async () =>{
-        if(params.id){
+    const handleDelete = async () => {
+        if (params.id) {
             const status = await axios.delete("/api/products?id=" + params.id)
+            
+            // delete all images from storage cloud 
+            for (const img of product?.images) {
+                const key = img.split("/").pop()
+                await axios.delete("/api/uploadthing?id=" + key)
+            }
 
-            if(status.status === 200){
+            if (status.status === 200) {
                 alert("Deleted Successfully")
-                router.push('/products')
-            }else{
+                goBack()
+            } else {
                 alert("server error, let the engineer know it")
             }
         }
     }
+    console.log(product)
 
-    const goBack = ()=>{
+    const goBack = () => {
         router.push("/products")
     }
     return (
         <>
             <h1 className="text-center">Are you sure, you want to develte this product ?</h1>
 
+            <div className="flex justify-center">
+                {product?.images?.map(im => (
+                    <Image key={im} src={im} width={100} height={100} alt="product img" />
+                ))
+
+                }
+            </div>
+
             <div className="text-center my-5">
                 <h1>Title : {product?.title}</h1>
                 <h1>Description : {product?.description}</h1>
                 <h1>Price : {product?.price}</h1>
+                <h1>Catagory: {product?.catagory || " "}</h1>
             </div>
 
             <div className="flex gap-2 justify-center">
@@ -48,7 +65,7 @@ const DeleteProduct = ({ params }) => {
                 <button className="btn-default" onClick={goBack}>No</button>
             </div>
 
-            
+
         </>
     )
 }
