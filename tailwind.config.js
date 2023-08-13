17ece9bf65ddc1634d09a3f3eb58e087a31d09af
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-13 06:26:23 +0600

    updated navbar style, color, tailwind.config.js file added new colors added; ham menu show on mobile and hide on md to lg display, navbar hide on mobile display and show on mb to lg display

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#5542F6"
+        primary: "#5542F6",
+        highlight: "#eae8fb",
+        bgGray: "#fbfafd",
       }
     },
   },
