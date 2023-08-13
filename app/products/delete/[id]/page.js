f99c2bea43709a31fd0690a9a035d705e5e6090a
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-08-13 07:47:41 +0600

    product and catagory edit and delete button updated

--- app/products/delete/[id]/page.js
+++ app/products/delete/[id]/page.js
@@ -60,7 +60,7 @@ const DeleteProduct = ({ params }) => {
             </div>
 
             <div className="flex gap-2 justify-center">
-                <button className="btn-red" onClick={handleDelete}>Yes</button>
+                <button className="btn-reject" onClick={handleDelete}>Yes</button>
                 <button className="btn-default" onClick={goBack}>No</button>
             </div>
 
