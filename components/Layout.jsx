17a0d68509cc9a64e5083afbbb815570b9b42b90
Md Antor <md.antor1155@gmail.com>
2024-03-28 16:05:27 +0600

    layout.jsx page css type resolved

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -27,7 +27,7 @@ export default function Layout({ children }) {
   return (
     <div className='bg-bgGray min-h-screen '>
       {/* this part is the nav bar in mobile  */}
-      <div className='block md:hidden flex items-center p-4'>
+      <div className='md:hidden flex items-center p-4'>
         <button onClick={() => setShowNav(true)}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
