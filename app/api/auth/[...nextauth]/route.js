873d36dc4cd831efd54d34344580c23639c99ace
Md Antor <md.antor1155@gmail.com>
2024-03-28 16:03:51 +0600

    given value to login with credentials

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import GoogleProvider from "next-auth/providers/google"
 
 import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "../../../../database/mongodb"
@@ -8,27 +9,49 @@ import clientPromise from "../../../../database/mongodb"
 const adminEmails = ["md.antor1155@gmail.com"]
 
 const handler = NextAuth({
+  // pages: {
+  //   signIn: "/auth",
+  // },
   providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+    CredentialsProvider({
+      name: "admin Credentials",
+      type: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", value:"md.antor1155@gmail.com" },
+        password: { label: "Password", type: "password", value: "password123" }
+      },
+      authorize: async (credentials, req) => {
+        // Your email and password authentication logic here
+        // This is a basic example. Replace it with your actual logic and encripted passwart matching library like argon2.
+        if (credentials.email === 'md.antor1155@gmail.com' && credentials.password === 'password123') {
+          // return Promise.resolve({ email: 'test@example.com' });
+          return {id: "123", name: "admin", email: "md.antor1155@gmail.com"};
+        } else {
+          // return Promise.resolve(null);
+          return null;
+        }
+      }
+    })
+    //     GoogleProvider({
+    //       clientId: process.env.GOOGLE_CLIENT_ID,
+    //       clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    //     })
   ],
 
-  adapter: MongoDBAdapter(clientPromise),
+  // adapter: MongoDBAdapter(clientPromise),
 
-  callbacks: {
-    session: ({session, token, user}) =>{
+  // callbacks: {
+  //   session: ({session, token, user}) =>{
 
-      if (adminEmails.includes(user?.email)){
-        return session
-      } else{
-        return false
-      }
-    },
-  },
+  //     if (adminEmails.includes(user?.email)){
+  //       return session
+  //     } else{
+  //       return false
+  //     }
+  //   },
+  // },
 
 })
 
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -16,8 +16,8 @@ export default function Layout({ children }) {
       <div className='bg-gray-500 w-screen h-screen flex items-center'>
         <div className='text-center w-full'>
           <button
-            className='bg-white p-2 px-4 rounded-lg' onClick={() => signIn("google")}>
-            Login with google
+            className='bg-white p-2 px-4 rounded-lg' onClick={() => signIn()}>
+            Login Please
           </button>
         </div>
       </div>
